2cbc9e79d3ef21f648672c11b470cafba9347ca4
skjgituser <singhaishobhit94@email.com>
2025-11-05 06:09:26 +0000

    added s3 & dDB lock

--- providers.tf
+++ providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "shobhit-roche-terraform-bucket"
+    key = "dev/shobhit/terraform.tfstate"
+    dynamodb_table = "shobhit-roche-table-1"
+    region = "ap-southeast-2"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
