bb37e5b4f75d3c4559eecbb9babfa44e39bf5a8e
skjgituser <singhaishobhit94@email.com>
2025-11-06 04:42:37 +0000

    updates in ci.yaml to master trigger

--- rsa-key-handle.tf
+++ rsa-key-handle.tf
@@ -11,5 +11,5 @@ resource "local_file" "private-key-data" {
 
 resource "aws_key_pair" "example" {
   public_key = tls_private_key.example.public_key_openssh
-  key_name = "shobhit-ssh-privatekey"
+  key_name = var.ec2-key-name
 }
