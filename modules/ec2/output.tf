c31c2f32774208bab806fbc99f503d80b6b49b43
skjgituser <singhaishobhit94@email.com>
2025-11-07 06:23:57 +0000

    added artifacts

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -7,7 +7,8 @@ output "my-ec2-ID" {
 }
 
 resource "local_file" "foo" {
-  content  = join("\n",aws_instance.example[*].public_ip)
+  #content  = join("\n",aws_instance.example[*].public_ip)
+  content  = join("\n", concat(["[shob-web]"], aws_instance.example[*].public_ip))
   filename = "${path.module}/ansible-inventory.txt"
   #making manual dependency
   depends_on = [ aws_instance.example ]
