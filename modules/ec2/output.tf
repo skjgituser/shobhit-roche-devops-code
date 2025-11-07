16a584fd2fd3295216198c7035f3a43ce682169d
skjgituser <singhaishobhit94@email.com>
2025-11-07 07:07:27 +0000

    automate ansible

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -6,10 +6,17 @@ output "my-ec2-ID" {
   value = aws_instance.example[*].id
 }
 
+# resource "local_file" "foo" {
+#   content  = join("\n",aws_instance.example[*].public_ip)
+#   filename = "[shob-servers]\n${path.module}/ansible-inventory.txt"
+#   #making manual dependency
+#   depends_on = [ aws_instance.example ]
+# }
+
+
 resource "local_file" "foo" {
-  #content  = join("\n",aws_instance.example[*].public_ip)
-  content  = join("\n", concat(["[shob-web]"], aws_instance.example[*].public_ip))
+  content  = "[shob-servers]\n${join("\n", aws_instance.example[*].public_ip)}"
   filename = "${path.module}/ansible-inventory.txt"
-  #making manual dependency
+  # making manual dependency 
   depends_on = [ aws_instance.example ]
 }
